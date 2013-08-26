1a06e68a61d4884301707fe47a186d644c6f666e
Danial Pearce <git@tigris.id.au>
2013-08-27 00:11:00 +1000

    RDoc for board

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,19 @@
 class Board
+  # Creates a board object
+  #
+  # @param [Integer] Maximum number of squares on the X axis
+  # @param [Integer] Maximum number of squares on the Y axis
+  # @return [Board]
   def initialize x, y
     @max_x = x
     @max_y = y
   end
 
+  # Determine if an object of any kind can be placed at the given coordinates
+  #
+  # @param [Integer] X coordinate
+  # @param [Integer] Y coordinate
+  # @return [Boolean]
   def can_place? x, y
     x >= 0 and x < @max_x and y >= 0 and y < @max_y
   end
